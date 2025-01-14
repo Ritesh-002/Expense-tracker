d2d2846c9f6054e1e9590fd0673b6c1ecd310d2f
Ritesh-002 <chughritesh002@gmail.com>
2025-01-14 23:25:34 +0530

    Added isLoggedIn middleware

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken'
+function isLoggedIn(req, res, next) {
+    const { token } = req.cookies
+    if (!token) return res.status(401).json({ error: 'Access denied' });
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.userId = decoded.userId;
+        next();
+    } catch (error) {
+        res.status(401).json({
+            error: 'Invalid token'
+        });
+    }
+};
+
+export default isLoggedIn;
